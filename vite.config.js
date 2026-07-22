913d82c2dc52f79d7d8cac84a208981b5c0e8f31
anuprajwal <anuprajwal89@gmail.com>
2026-07-22 23:43:02 +0530

    added traffic alowing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 7000,
-    strictPort: true // Prevents Vite from automatically trying 7001 if 7000 is busy
+    strictPort: true, 
+    host: true,  
+    allowedHosts: [
+      'auth.docapp.co.in'
+    ]
   }
 })
